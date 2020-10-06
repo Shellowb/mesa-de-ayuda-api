b43d4b645e29e8e67335397f2e3f89e4e194b6c8
Pablo Arancibia Barahona <pabloarancibiabarahona@MacBook-Pro-de-Pablo.local>
2020-10-05 21:29:24 -0300

    added allowed ghost

--- API/settings.py
+++ API/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
+ALLOWED_HOSTS = ['mesadeayudadcc.herokuapp.com', '127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
